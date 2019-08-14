a3a7c6d3dc9e403dbd2acbb6f7c550ccac6f4e71
Adella <adellakenn@gmail.com>
2019-08-14 19:49:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
 require './environment'
 
 module FormsLab
-  class App < Sinatra::Base
+	class App < Sinatra::Base
+		# code other routes/actions here
+		get '/' do
+			'Welcome to the Nested Forms Lab!'
+		end
+		get '/new' do
+			erb :'pirates/new'
+		end
 
-    # code other routes/actions here
+		post '/pirates' do
+			# returns 200 with passing args in the models
+			@pirate = Pirate.new(params[:pirate])
+			params[:pirate][:ships].each { |details| Ship.new(details) }
+			@ships = Ship.all
 
-  end
+			erb :'pirates/show'
+		end
+	end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
-end
+	attr_accessor :name, :weight, :height
+	@@all = []
+	def initialize(args)
+		@name = args[:name]
+		@weight = args[:weight]
+		@height = args[:height]
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+end # end of Class
