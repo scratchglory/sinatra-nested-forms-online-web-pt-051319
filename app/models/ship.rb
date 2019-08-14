de8a27647e0a677fe3bd33426603ff800a4776f8
Adella <adellakenn@gmail.com>
2019-08-14 19:48:36 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
-end
+	attr_accessor :name, :type, :booty
+	@@all = []
+
+	def initialize(args)
+		@name = args[:name]
+		@type = args[:type]
+		@booty = args[:booty]
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.clear
+		@@all = []
+	end
+end # end of Class
